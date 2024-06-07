726be8ecf54c5f3e40ae9583f71ab63e4849e29c
paulcaruana <paul.caruana@objective.com>
2024-06-07 18:52:25 +1000

    Add user route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,16 @@
 import {NextRequest, NextResponse} from "next/server";
 import {userService} from "@/server/services/serviceContainer";
+import {User} from "@/model/user";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
    return NextResponse.json(await userService.fetchAll());
+}
+
+export async function POST(
+  req: NextRequest,
+  { user }: { user: User }
+) {
+   return NextResponse.json(await userService.create(user));
 }
