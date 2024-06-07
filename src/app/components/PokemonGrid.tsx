c948a3894c844a0545120f8c7da8c66b0a82182d
paulcaruana <paul.caruana@objective.com>
2024-06-07 17:22:03 +1000

    Init

--- src/app/components/PokemonGrid.tsx
+++ src/app/components/PokemonGrid.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
-
-import { PokemonCard } from "./PokemonCard";
-import { Pokemon } from "@/types";
+import {PokemonDetail} from "@/routes";
+import {PokemonCard} from "./PokemonCard";
+import {Pokemon} from "@/types";
 
 export function PokemonGrid({ pokemon }: { pokemon: Pokemon[] }) {
   return (
     <div className="flex flex-wrap">
       {pokemon.map((p) => (
-        <Link href={`/pokemon/${p.id}`} key={p.id}>
+        <PokemonDetail.Link pokemonId={p.id} key={p.id}>
           <PokemonCard pokemon={p} />
-        </Link>
+        </PokemonDetail.Link>
       ))}
     </div>
   );
