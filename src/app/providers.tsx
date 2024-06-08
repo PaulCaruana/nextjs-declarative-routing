6c10c6c22d2931d1528e4d2b59a93f764d153661
paulcaruana <paul.caruana@objective.com>
2024-06-08 16:24:46 +1000

    Add providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,23 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { useState } from "react";
+import theme from "@/app/theme/theme";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [client] = useState(new QueryClient());
 
   return (
-    <QueryClientProvider client={client}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <QueryClientProvider client={client}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
