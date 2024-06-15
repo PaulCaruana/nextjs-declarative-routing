6701ec833fec9c25d0db582c62550c59fa99fd0f
paulcaruana <paul.caruana@objective.com>
2024-06-15 16:48:46 +1000

    Refactor names

--- src/app/components/mui/index.tsx
+++ src/app/components/mui/index.tsx
@@ -1,3 +1,3 @@
-export { default as Centre } from "@/library/surfaces/containers/Centre";
-export { default as Container } from "@/library/surfaces/containers/Container";
+export { default as Centre } from "@/library/layout/containers/Centre";
+export { default as Container } from "@/library/layout/containers/Container";
 export { default as Header } from "@/library/surfaces/headers/Header";
